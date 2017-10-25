f25be5c1dbce1e41c8033c8a334a7c15f5b64aa4
Pierce C Lauring <pclauring@gmail.com>
2017-10-25 15:34:45 -0400

    changed WriteLine to Write

--- Power_Table_Lab3/Program.cs
+++ Power_Table_Lab3/Program.cs
@@ -10,13 +10,14 @@ namespace Power_Table_Lab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter an integer: ");
+            Console.Write("Enter an integer: ");
 
             string input = Console.ReadLine();
             int.TryParse(input, out int numberTable);
 
             Console.WriteLine("Number Squared Cubed");
             Console.WriteLine("====== ======= =====");
+
             for (int i = 1; i < (numberTable+1); i++)
             {
                 {
