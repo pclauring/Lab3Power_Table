fc336701a32471012338a637f14120c8bfd7cbd0
Pierce C Lauring <pclauring@gmail.com>
2017-10-25 15:29:15 -0400

    first commit

---
+++ Power_Table_Lab3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power_Table_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+
+            string input = Console.ReadLine();
+            int.TryParse(input, out int numberTable);
+
+            Console.WriteLine("Number Squared Cubed");
+            Console.WriteLine("====== ======= =====");
+            for (int i = 1; i < (numberTable+1); i++)
+            {
+                {
+                    Console.WriteLine(i + "        " + (i * i) + "       " + Math.Pow(i, 3));
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Lab3Power_Table
